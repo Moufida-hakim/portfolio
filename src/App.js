89e97b56eee9b0eb32daa27a1acea287bd5defe5
Moufida Hakim <moufida.hakim.23@gmail.com>
2022-09-11 15:43:25 +0100

    Set up bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-
+import Navbar from'./components/Navbar'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-         Moufida Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Navbar />
     </div>
   );
 }
