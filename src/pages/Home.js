89f776acd161b70175356f3870ba649734b02a7c
Moufida Hakim <moufida.hakim.23@gmail.com>
2022-09-17 17:18:41 +0100

    Add project pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import webImage from '../images/web.png'
 
 function Home() {
   return (
-    <div>
+    <div className='alert alert-primary'>
     <img src="/images/my-photo.jpg" alt="My photo" />
     <h1>hello my name is Moufida. I am a Fullstack Web Developer</h1>
     <img src={webImage} alt="web Development" />
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,30 @@
 import React from 'react'
-
+const myProjects = [{
+  title: 'Business Land',
+  description: 'A simple landing page developed with HTML and CSS',
+  url: ' https://moufida-hakim.github.io/business-land/'
+}]
+// {
+//   title: 'to doom',
+//   description: 'A simple to do list developed with HTML and CSS',
+//   url: ' https://moufida-hakim.githu.io/business-land/'
+// }
 function Projects() {
   return (
-    <div>Projects</div>
+    <div>
+      {myProjects.map((item, index) => (
+        <div style={{ padding: 20 }}>
+          <div class="card" >
+            <div class="card-body">
+              <h5 class="card-title">{item.title}</h5>Some
+              <p class="card-text"> {item.description}</p>
+              <a href={item.url} target='_blank'class="card-link">Preview</a>
+            </div>
+          </div>
+        </div>
+
+      ))}
+    </div>
   )
 }
 
