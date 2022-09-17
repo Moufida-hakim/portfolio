da38e586d87322206b35e3e27f0fa35683ee716b
Moufida Hakim <moufida.hakim.23@gmail.com>
2022-09-17 15:23:44 +0100

    complete the contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,31 @@
 import React from "react";
+const informations = {
+    email: 'moufida.hakim.23@gmail.com',
+    phone: '+216 92 945 387',
+    address: {
+        country: 'tunisie',
+        city: 'sfax',
+        street: 'Avenue Mohamed Jammoussi',
+        postalCode: 3000
+    }
+}
 function Contact() {
-return ( 
-    <div>
-        Contact
-    </div>
-)    
+    return (
+        <div>
+            <div className="contact-item">
+                <i className='fa-regular fa-envelope'></i>
+                <p className='contact-text'> {informations.email}</p>
+            </div>
+            <div className="contact-item">
+                <i className="fa-solid fa-phone"></i>
+                <p className='contact-text'> {informations.phone}</p>
+
+            </div>
+            <div> 
+            <i class="fa-solid fa-location-dot"></i>
+                <p> {informations.address.street} {informations.address.country} {informations.address.city}</p>
+            </div>
+        </div>
+    )
 }
 export default Contact;
